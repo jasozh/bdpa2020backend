0e481ec46585ceddd409bcf5d6adce901781fee4
unknown <jasonz4200@gmail.com>
2020-08-05 15:47:23 -0500

    Improve spacing for JSON data

--- schemas/userInformation.js
+++ schemas/userInformation.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose")
 
 const userInformationSchema = mongoose.Schema({
-  title: { type: String },
-  username: { type: String, required: true, unique: true },
-  suffix: { type: String },
-  firstName: { type: String, required: true },
+  title     : { type: String },
+  username  : { type: String, required: true, unique: true },
+  suffix    : { type: String },
+  firstName : { type: String, required: true },
   middleName: { type: String },
-  lastName: { type: String, required: true },
-  sex: { type: String, required: true },
-  birthdate: { type: String, required: true },
-  city: { type: String },
-  state: { type: String },
-  zip: { type: String },
-  country: { type: String },
-  email: { type: String, required: true},
-  phone: { type: String}
+  lastName  : { type: String, required: true },
+  sex       : { type: String, required: true },
+  birthdate : { type: String, required: true },
+  city      : { type: String },
+  state     : { type: String },
+  zip       : { type: String },
+  country   : { type: String },
+  email     : { type: String, required: true},
+  phone     : { type: String}
 })
 
 const model = mongoose.model("userInformation", userInformationSchema)
