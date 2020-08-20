71890282ac530ae3fd5ab0d8aaafde42584c87c5
JosephMugo <joemu18@gmail.com>
2020-08-20 14:21:05 -0500

    Admin can now edit ban through dashboard

--- userServices.js
+++ userServices.js
@@ -161,14 +161,16 @@ const returnDeletedUser = async (req, res) => {
     }
 }
 // User Ban 
-const returnUserBan = async email => {
-    const userBan = await userModel.find({ email }, { isBanned: 1, _id: 0 });
+const changeUserBan = async email => {
+    const userBan = await userInformationModel.find({ email }, { isBanned: 1, _id: 0 });
     if (userBan === null || userBan === undefined) return undefined
-    console.log("Found user ban status", userBan)
-    return userBan
+    const currentState = await userBan[0].isBanned
+    await userInformationModel.updateOne({ email }, { $set: {isBanned: !currentState}})
+    return true
 }
 const requestUserBan = async (req, res) => {
     console.log('Requesting Ban')
     console.log(req.params.email)
+    await changeUserBan(req.params.email)
 }
 module.exports = { addUserToDatabase, verifyUserCredentials, verifyUserSecurityQuestions, findUser, returnUserInformation, updateUserInformation, returnUserRole, returnAllUsers, returnDeletedUser, requestUserBan } 
